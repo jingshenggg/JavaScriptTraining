0c8c96fbc84e9829a7ceb70c0b942e04149fdb31
jingsheng.tey <jingsheng.tey@anacle.com>
2023-12-03 21:18:54 +0800

    Coding challenge #2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -73,3 +73,39 @@ to
 make 
 new
 lines.`);
+
+
+////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, saying who has the higher BMI. The message can be either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's BMI (28.3) is higher than John's (23.9)!"
+
+HINT: Use an if/else statement 😉
+
+GOOD LUCK 😀
+*/
+
+solution:
+let height;
+let mass;
+let BMI;
+function CalculateBMI (mass, height){
+    BMI = mass / (height * height);
+    return BMI;
+}
+
+let markBMI = CalculateBMI(78, 1.69);
+let johnBMI = CalculateBMI(95, 1.88);
+
+if (markBMI > johnBMI)
+{
+    console.log(`Mark's BMI ${(markBMI)} is higher than John's ${(johnBMI)}!`);
+}
+else 
+{
+    console.log(`Mark's BMI ${(markBMI)} is higher than John's ${(johnBMI)}!`);
+}
