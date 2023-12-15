14eaaf8fc7f1db0b6f047e0ad2840eb9253d27c0
jingsheng.tey <jingsheng.tey@anacle.com>
2023-12-15 17:41:43 +0800

    Update script.js
    Stop at lesson 41

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -49,26 +49,100 @@ HINT: To check if number A is at least double number B, check for A >= 2 * B. Ap
 GOOD LUCK 😀
 */
 
-function calcAverage(score1, score2, score3) {
-    const avgScore = (score1+score2+score3)/3;
-    return (avgScore);
-}
-const dolphinAvg = calcAverage(44,1000,71);
-const koalasAvg = calcAverage(65,54,49);
+// function calcAverage(score1, score2, score3) {
+//     const avgScore = (score1+score2+score3)/3;
+//     return (avgScore);
+// }
+// const dolphinAvg = calcAverage(44,1000,71);
+// const koalasAvg = calcAverage(65,54,49);
  
-const checkWinner = function checkWinner(dolphinAvg, koalasAvg) {
-    if (dolphinAvg >= 2 * koalasAvg){
-        console.log(`Dolphins win (${dolphinAvg} vs. ${koalasAvg})!`);
-    }
-    else if (koalasAvg >= 2*dolphinAvg){
-        console.log(`Koalas win (${koalasAvg} vs. ${dolphinAvg})!`);
-    }
-    else{
-        console.log('No team wins...');
-    }
-}
-checkWinner(dolphinAvg, koalasAvg);
+// const checkWinner = function checkWinner(dolphinAvg, koalasAvg) {
+//     if (dolphinAvg >= 2 * koalasAvg){
+//         console.log(`Dolphins win (${dolphinAvg} vs. ${koalasAvg})!`);
+//     }
+//     else if (koalasAvg >= 2*dolphinAvg){
+//         console.log(`Koalas win (${koalasAvg} vs. ${dolphinAvg})!`);
+//     }
+//     else{
+//         console.log('No team wins...');
+//     }
+// }
+// checkWinner(dolphinAvg, koalasAvg);
 
 // Lesson 36 Array
 // const exampleArray = []; -> values inside can be changed even though it is declared as const because array is not primitive, only primitive values are immutable for const.
 
+// Lesson 37 Array methods
+//const friends = ['Michael', 'Steven', 'Peter'];
+// friends.push('Jay');
+//console.log(friends);
+//const newLengthOfArray = friends.push('Jay'); // if store the array methods, it will become the lenght.
+//console.log(newLengthOfArray);
+//friends.unshift('Alex'); //add element to first position, .pop() -> to remove element, no need to pass in parameters and it will pop from last element.
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/*
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+2. And now let's use arrays! So create an array 'bills' containing the test data below.
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
+4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
+
+TEST DATA: 125, 555 and 44
+
+HINT: Remember that an array needs a value in each position, and that value can actually be the returned value of a function! So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+
+GOOD LUCK 😀
+*/
+
+// const calcTip = function (bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//   }
+// const bills = [125,555,44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(bills, tips);
+
+// Lesson 40 Dot vs Square Bracket
+// dot is used to assess properties, bracket can take in expression.
+
+// Lesson 41 Object
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYeah: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: false,
+  
+    // calcAge: function (birthYeah) {
+    //   return 2037 - birthYeah;
+    // }
+  
+    // calcAge: function () {
+    //   // console.log(this);
+    //   return 2037 - this.birthYeah;
+    // }
+  
+    calcAge: function () {
+      this.age = 2037 - this.birthYeah;
+      return this.age;
+    },
+  
+    getSummary: function () {
+      return `${this.firstName} is a ${this.calcAge()}-year old ${jonas.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license.`
+    }
+  };
+  
+  console.log(jonas.calcAge());
+  
+  console.log(jonas.age);
+  console.log(jonas.age);
+  console.log(jonas.age);
+  
+  // Challenge
+  // "Jonas is a 46-year old teacher, and he has a driver's license"
+  console.log(jonas.getSummary());
